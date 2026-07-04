a5a582119fdcd7a3aea430399a6fcbb9132dfa1b
Archana67-T <archanatcse06@gmail.com>
2026-07-04 17:29:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+Date,Technology,Job_Postings,Average_Salary
+2025-01-01,Python,120,650000
+2025-02-01,Python,135,670000
+2025-03-01,Python,150,690000
+2025-04-01,Python,165,710000
+2025-05-01,Python,180,730000
+2025-06-01,Python,195,750000
+2025-01-01,Artificial Intelligence,90,800000
+2025-02-01,Artificial Intelligence,105,830000
+2025-03-01,Artificial Intelligence,125,860000
+2025-04-01,Artificial Intelligence,145,900000
+2025-05-01,Artificial Intelligence,170,950000
+2025-06-01,Artificial Intelligence,200,1000000
+2025-01-01,Cloud Computing,110,750000
+2025-02-01,Cloud Computing,118,760000
+2025-03-01,Cloud Computing,130,780000
+2025-04-01,Cloud Computing,142,800000
+2025-05-01,Cloud Computing,155,820000
+2025-06-01,Cloud Computing,170,850000
+2025-01-01,Cybersecurity,80,700000
+2025-02-01,Cybersecurity,88,720000
+2025-03-01,Cybersecurity,95,740000
+2025-04-01,Cybersecurity,108,760000
+2025-05-01,Cybersecurity,120,790000
+2025-06-01,Cybersecurity,135,820000
+2025-01-01,Data Science,100,780000
+2025-02-01,Data Science,110,800000
+2025-03-01,Data Science,122,820000
+2025-04-01,Data Science,135,850000
+2025-05-01,Data Science,145,870000
+2025-06-01,Data Science,160,900000
